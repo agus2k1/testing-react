4753a420f80d41a3cc953c4421e0c55a1bb547ec
West <agus2kcoin@outlook.com>
2022-11-27 13:44:13 -0300

    useState object destructuring example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css"
 
 function App() {
+  const [person, setPerson] = useState({
+    name: "Roy",
+    age: 24,
+    occupation: "CEO"
+  });
+
+  const changeName = () => {
+    let newPerson = {...person, name: "Agus"};
+    setPerson(newPerson);
+  }
+
   return (
-    null
+    <div className="container">
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.occupation}</h3>
+      <button onClick={changeName}></button>
+    </div>
   );
 }
 
